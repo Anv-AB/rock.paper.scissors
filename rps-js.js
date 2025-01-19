64fd676e04ef03a83e780bff2baa97b5d2e35a9e
Anv-AB <abdullahanvar03@gmail.com>
2025-01-20 00:03:13 +1100

    Created Branch for ui Buttons created, event listeners added Logic for 5 round limit added Score counter and final result displays added

--- rps-js.js
+++ rps-js.js
@@ -10,64 +10,77 @@ let getComputerChoice = () => {
   }
 };
 
-// Human Choice
-let getHumanChoice = () => {
-  let humanPrompt = prompt("Rock, Paper or Scissors?")
-  return humanPrompt
-}
+// Score Initialiser
+let computerScore = 0;
+let humanScore = 0;
+let round = 0;
 
 
-// Playing the game
-let playGame = () => {
-
-  // Score Initialisation
-  let computerScore = 0;
-  let humanScore = 0;
-  
-  for (let i = 0; i < 5; i++) {
-
-    // Playing a round
-    let playRound = (playRoundHuman, playRoundComputer) => {
-      const outcome = `${playRoundHuman}-${playRoundComputer}`
-      switch (outcome) {
-        case "rock-scissors":
-          ++humanScore
-          return "You win! Rock beats Scissors.";
-        case "paper-rock":
-          ++humanScore
-          return "You win! Paper beats Rock."
-        case "scissors-paper":
-          ++humanScore
-          return "You win! Scissors beats Paper."
-        case "scissors-rock":
-          ++computerScore
-          return "You lose! Rock beats Scissors.";
-        case "rock-paper":
-          ++computerScore
-          return "You lose! Paper beats Rock."
-        case "paper-scissors":
-          ++computerScore
-          return "You lose! Scissors beats Paper."
-        case "rock-rock":
-        case "paper-paper":
-        case "scissors-scissors":
-          return "It's a tie!";
-        default:
-          return "Invalid input! Please select Rock, Paper or Scissors!";
-      }
-    }
-
-    const humanSelection = getHumanChoice().toLowerCase();
-    const computerSelection = getComputerChoice().toLowerCase();
-
-    console.log(`You have selected: ${humanSelection}`);
-    console.log(`The computer has selected: ${computerSelection}`);
-    console.log(playRound(humanSelection, computerSelection))
-    console.log(humanScore)
-    console.log(computerScore)
+// Single Round Logic
+let playRound = (playRoundHuman, playRoundComputer) => {
+  const outcome = `${playRoundHuman}-${playRoundComputer}`
+  switch (outcome) {
+    case "rock-scissors":
+      ++humanScore
+      return "You win! Rock beats Scissors.";
+    case "paper-rock":
+      ++humanScore
+      return "You win! Paper beats Rock."
+    case "scissors-paper":
+      ++humanScore
+      return "You win! Scissors beats Paper."
+    case "scissors-rock":
+      ++computerScore
+      return "You lose! Rock beats Scissors.";
+    case "rock-paper":
+      ++computerScore
+      return "You lose! Paper beats Rock."
+    case "paper-scissors":
+      ++computerScore
+      return "You lose! Scissors beats Paper."
+    case "rock-rock":
+    case "paper-paper":
+    case "scissors-scissors":
+      return "It's a tie!";
+    default:
+      return "Invalid input! Please select Rock, Paper or Scissors!";
   }
-
-return `Game Over! Final Scores - You: ${humanScore}, Computer: ${computerScore}`;
-
 }
-console.log(playGame())
+
+// Document Query Selectors
+let selection = document.querySelector("#selection");
+let human = document.querySelector("#human");
+let computer = document.querySelector("#computer");
+let result = document.querySelector("#result");
+let humanScoreKeeper = document.querySelector("#humanScoreKeeper");
+let computerScoreKeeper = document.querySelector("#computerScoreKeeper");
+let finalResult = document.querySelector("#finalResult")
+
+
+// Event listener for buttons
+selection.addEventListener("click", function handleClick(e) {
+  if (e.target.tagName === "BUTTON") {
+    const humanSelection = e.target.textContent;
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(humanSelection.toLowerCase(), computerSelection.toLowerCase());
+
+    human.textContent = `You have selected: ${humanSelection}`
+    computer.textContent = `The computer has selected: ${computerSelection}`
+    result.textContent = `Round ${++round}: ${roundResult}`
+    humanScoreKeeper.textContent = `Your score: ${humanScore}`
+    computerScoreKeeper.textContent = `Opponent score: ${computerScore}`
+
+    if (round === 5) {
+      const finalMessage =
+        humanScore > computerScore
+        ? "Congratulations, you won!"
+        : computerScore > humanScore
+        ? "Unfortunately, you've lost!!!"
+        : "It's a draw!!!";
+
+      finalResult.textContent += `Final Result: ${finalMessage}`;
+      selection.removeEventListener("click", handleClick);
+    }
+  }
+}
+);
